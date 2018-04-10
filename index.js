6dcfdf6bc4a30eeafdd7587af128200c83b4b003
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-04-10 16:51:36 +0200

    conf consume

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var util = require('util')
 var zlib = require('zlib')
 var tar = require('tar-fs')
 var pump = require('pump')
+var lpstream = require('length-prefixed-stream')
 
 // TODO: 2 conf obj, length-prefix stream, initially send dirpath opt with onlies
 
@@ -36,26 +37,43 @@ function FilePlug (opts, onconsumer) {
   var self = this
 
   this.on('connection', function (socket) {
-    socket.on('data', function (buf) {
-      var filepath = buf.toString()
+    var decoder = lpstream.decode()
+    pump(socket, decoder)
+    // socket.on('data', function (buf) {
+    decoder.on('data', function (buf) {
+      // var filepath = buf.toString()
+      var preflight
+      try {
+        preflight = JSON.parse(buf.toString())
+      } catch (err) {
+        return onconsumer(err)
+      }
 
-      if (self._opts.strict && !self._whitelist.has(filepath)) {
+      // refactor 2 allow
+      if (self._opts.strict && !self._whitelist.has(preflight.path)) {
         return onconsumer('illegal request 4 non-whitelisted resource')
       }
 
-      stat(filepath, self._opts, function (err, stats) {
+      // stat(filepath, self._opts, function (err, stats) {
+      stat(preflight.path, self._opts, function (err, stats) {
         if (err) return onconsumer(err)
 
         var readStream
-        if (stats.isDirectory()) readStream = tar.pack(filepath)
-        else if (stats.isFile()) readStream = fs.createReadStream(filepath)
-        else return onconsumer('unsupported resource')
+        if (stats.isDirectory() && preflight.only) {
+          readStream = tar.pack(preflight.path, { entries: preflight.only })
+        } else if (stats.isDirectory()) {
+          readStream = tar.pack(preflight.path)
+        } else if (stats.isFile()) {
+          readStream = fs.createReadStream(preflight.path)
+        } else {
+          return onconsumer('unsupported resource')
+        }
 
         var gzip = zlib.createGzip()
         pump(readStream, gzip, socket, function (err) {
           if (err) return onconsumer(err)
           self._supplied++
-          onconsumer(null, filepath)
+          onconsumer(null, preflight.path)
         })
 
         gzip.on('readable', function () {
@@ -79,28 +97,40 @@ FilePlug.prototype.__defineGetter__('consumed', function () {
   return this._consumed
 })
 
-function consume (port, host, type, filepath, mypath, callback) {
+// function consume (port, host, type, filepath, mypath, callback) {
+function consume (conf, callback) {
   if (!callback) callback = noop
   var self = this
 
-  var socket = net.connect(port, host, function () {
-    socket.write(filepath, function () {
+  // var socket = net.connect(port, host, function () {
+  var socket = net.connect(conf.port, conf.host, function () {
+    var encoder = lpstream.encode()
+    pump(encoder, socket)
+    // socket.write(filepath, function () {
+    var preflight = JSON.stringify({
+      path: conf.filepath,
+      only: conf.type === 'file' ? null : conf.only
+    })
+    // encoder.write(conf.filepath, function () {
+    encoder.write(preflight, function () {
 
+      var target = conf.type === 'file' ? conf.mypath : conf.mypath + '.tar'
       var writeStream =
-        fs.createWriteStream(type === 'file' ? mypath : mypath + '.tar')
+        // fs.createWriteStream(type === 'file' ? mypath : mypath + '.tar')
+        fs.createWriteStream(target)
 
       pump(socket, zlib.createGunzip(), writeStream, function (err) {
         if (err) return callback(err)
         self._consumed++
-        if (type === 'file') {
-          callback(null, mypath)
+        if (conf.type === 'file') {
+          callback(null, conf.mypath)
         } else {
-          var tarStream = fs.createReadStream(mypath + '.tar')
-          pump(tarStream, tar.extract(mypath), function (err) {
+          var tarStream = fs.createReadStream(conf.mypath + '.tar')
+          pump(tarStream, tar.extract(conf.mypath), function (err) {
             if (err) return callback(err)
-            fs.unlink(mypath + '.tar', function (err) {
+            fs.unlink(conf.mypath + '.tar', function (err) {
               if (err) return callback(err)
-              callback(null, mypath)
+              callback(null, conf.mypath)
             })
           })
         }
--- test.js
+++ test.js
@@ -8,126 +8,148 @@ var selfie = __filename
 var dope = selfie + ' yea!'
 var coke = selfie + ' yay!'
 var stash = path.join(__dirname, 'node_modules')
-var dopedir = stash + ' yea!'
+var dopedir = stash + '_yea!'
 var bad = selfie.substr(0, 5)
+var only = stash + '_only'
 
 tape.onFinish(function () {
-  del.sync([ dope, coke, dopedir ])
+  del.sync([ dope, coke, dopedir, only ])
 })
 
 tape('file sharing', function (t) {
-
   var a = fsPlug({ strict: false })
   var b = fsPlug()
-
   a.listen(10000, '127.0.0.1', function () {
-
-    b.consume(10000, '127.0.0.1', 'file', selfie, dope, function (err) {
+    var conf = {
+      port: 10000,
+      host: 'localhost',
+      type: 'file',
+      filepath: selfie,
+      mypath: dope
+    }
+    // b.consume(10000, '127.0.0.1', 'file', selfie, dope, function (err) {
+    b.consume(conf, function (err) {
       if (err) t.end(err)
-
       a.close()
-
       t.ok(fs.existsSync(dope), 'file shared')
       t.same(fs.readFileSync(selfie), fs.readFileSync(dope), 'identical files')
       t.is(a.supplied, 1, 'a should have supplied 1 file')
       t.is(b.consumed, 1, 'b should have consumed 1 file')
-
       t.end()
     })
-
   })
-
 })
 
 tape('dir sharing', function (t) {
-
   var a = fsPlug({ strict: false })
   var b = fsPlug()
-
   a.listen(10000, '127.0.0.1', function () {
-
-    b.consume(10000, '127.0.0.1', 'directory', stash, dopedir, function (err) {
+    var conf = {
+      port: 10000,
+      host: 'localhost',
+      type: 'directory',
+      filepath: stash,
+      mypath: dopedir
+    }
+    // b.consume(10000, '127.0.0.1', 'directory', stash, dopedir, function (err) {
+    b.consume(conf, function (err) {
       if (err) t.end(err)
-
       a.close()
-
       t.ok(fs.existsSync(dopedir), 'directory shared')
       t.same(fs.readdirSync(stash), fs.readdirSync(dopedir), 'identical dirs')
       t.is(a.supplied, 1, 'a should have supplied 1 dir')
       t.is(b.consumed, 1, 'b should have consumed 1 dir')
-
       t.end()
     })
-
   })
-
 })
 
 tape('exceptions', function (t) {
-
   t.plan(1)
-
   var a = fsPlug({ strict: false })
   var b = fsPlug()
-
   a.listen(10000, '127.0.0.1', function () {
-
-    b.consume(10000, '127.0.0.1', 'file', bad, dope, function (err) {
-
+    var conf = {
+      port: 10000,
+      host: 'localhost',
+      type: 'file',
+      filepath: bad,
+      mypath: dope
+    }
+    // b.consume(10000, '127.0.0.1', 'file', bad, dope, function (err) {
+    b.consume(conf, function (err) {
       a.close()
-
       t.ok(err, 'expecting a consume timeout error')
-
     })
-
   })
-
 })
 
 tape('in strict mode only whitelisted files are shared', function (t) {
-
   t.plan(1)
-
   var a = fsPlug({ strict: true })
   var b = fsPlug()
-
   a.listen(10000, '127.0.0.1', function () {
-
-    b.consume(10000, '127.0.0.1', 'file', selfie, coke, function (err) {
-
+    var conf = {
+      port: 10000,
+      host: 'localhost',
+      type: 'file',
+      filepath: selfie,
+      mypath: coke
+    }
+    b.consume(conf, function (err) {
+    // b.consume(10000, '127.0.0.1', 'file', selfie, coke, function (err) {
       a.close()
-
       t.ok(err, 'expecting a consume timeout error')
-
     })
-
   })
-
 })
 
 tape('events emit bytes written/read', function (t) {
-
   t.plan(1)
-
   var a = fsPlug({ strict: false })
   var b = fsPlug()
   var logA = []
   var logB = []
-
   a.on('bytes-supplied', logA.push)
   b.on('bytes-consumed', logB.push)
-
   a.listen(10000, '127.0.0.1', function () {
-
-    b.consume(10000, '127.0.0.1', 'file', selfie, coke, function (err) {
+    var conf = {
+      port: 10000,
+      host: 'localhost',
+      type: 'file',
+      filepath: selfie,
+      mypath: coke
+    }
+    // b.consume(10000, '127.0.0.1', 'file', selfie, coke, function (err) {
+    b.consume(conf, function (err) {
       if (err) t.end(err)
-
       a.close()
-
       t.same(logA, logB, 'written and read num bytes should be the same')
-
     })
-
   })
+})
 
+tape('only', function (t) {
+  t.plan(1)
+  var a = fsPlug({ strict: false })
+  var b = fsPlug()
+  a.listen(10000, '127.0.0.1', function () {
+    var conf = {
+      port: 10000,
+      host: 'localhost',
+      type: 'directory',
+      filepath: stash,
+      mypath: only,
+      only: [ 'tape' ]
+    }
+    b.consume(conf, function (err) {
+    // b.consume(10000, '127.0.0.1', 'file', selfie, coke, function (err) {
+      a.close()
+      fs.readdir(only, function (err, entries) {
+        if (err) t.end(err)
+        var ok = entries.length === 1 && entries[0] === 'tape'
+        t.ok(ok, 'should only contain tape')
+      })
+    })
+  })
 })
